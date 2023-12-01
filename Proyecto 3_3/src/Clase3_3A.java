fe4b7221c7f732ca964d2d470da617f8a9fcfb14
JhonatanGP <jguzman5292@ceuandalucia.es>
2023-12-01 16:44:38 +0100

    Clase3__3A

--- Proyecto 3_3/src/Clase3_3A.java
+++ Proyecto 3_3/src/Clase3_3A.java
@@ -1,9 +1,9 @@
-public class Clase3_3 {
+
+public class Clase3_3A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Practica 3_3 + Jhonatan");
-		
+
 	}
 
 }
