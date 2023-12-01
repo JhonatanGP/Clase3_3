dad6530e24f68d02e8b1366b5bd4dd49888cf00f
JhonatanGP <jguzman5292@ceuandalucia.es>
2023-12-01 17:36:13 +0100

    3

--- Proyecto 3_3/src/Clase3_3.java
+++ Proyecto 3_3/src/Clase3_3.java
@@ -2,7 +2,7 @@ public class Clase3_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Canela en rama");
+		System.out.println("Practica 3_3 + Jhonatan");
 		
 	}
 
